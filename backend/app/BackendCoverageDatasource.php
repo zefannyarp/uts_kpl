96e73d07e1d474aabcb383d5b5d46aec682296c7
ZEFANNYA RP <zefanya.pradipta@garasi.id>
2020-10-17 18:26:44 +0700

    correcting attributes name

--- backend/app/BackendCoverageDatasource.php
+++ backend/app/BackendCoverageDatasource.php
@@ -19,10 +19,10 @@ class BackendCoverageDatasource extends Model
     const ATTRIBUTE_NAMESPACE = 'namespace';
     const ATTRIBUTE_IGNORED_NAMESPACE = 'ignored_namespace';
     const ATTRIBUTE_TARGET_NAMESPACE = 'target_namespace';
-    const ATTRIBUTE_TARGET_BELOW_90percent = 'target_below_90pct';
+    const ATTRIBUTE_TARGET_BELOW_90PERCENT = 'target_below_90pct';
     const ATTRIBUTE_TARGET_COVERAGE = 'target_coverage';
-    const ATTRIBUTE_TARGET_BELOW_95percent = 'target_below_95pct';
-    const ATTRIBUTE_TARGET_COVERAGE_95plus = 'target_coverage_95pctplus';
+    const ATTRIBUTE_TARGET_BELOW_95PERCENT = 'target_below_95pct';
+    const ATTRIBUTE_TARGET_COVERAGE_95PLUS = 'target_coverage_95pctplus';
 
 
     protected $guarded;
@@ -39,9 +39,9 @@ class BackendCoverageDatasource extends Model
         self::ATTRIBUTE_NAMESPACE,
         self::ATTRIBUTE_IGNORED_NAMESPACE,
         self::ATTRIBUTE_TARGET_NAMESPACE,
-        self::ATTRIBUTE_TARGET_BELOW_90percent,
+        self::ATTRIBUTE_TARGET_BELOW_90PERCENT,
         self::ATTRIBUTE_TARGET_COVERAGE,
-        self::ATTRIBUTE_TARGET_BELOW_95percent,
-        self::ATTRIBUTE_TARGET_COVERAGE_95plus
+        self::ATTRIBUTE_TARGET_BELOW_95PERCENT,
+        self::ATTRIBUTE_TARGET_COVERAGE_95PLUS
     ];
 }
